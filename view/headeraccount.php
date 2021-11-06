a3d32671e92bbff07f3d3335dcd8a8202fcef791
0947286201 <tamtt1512@gmail.com>
2021-11-06 19:40:22 +0700

    no code push

--- view/headeraccount.php
+++ view/headeraccount.php
@@ -1,24 +1,27 @@
 <div class="header">
-		<nav class="navbar" style="padding: 5">
-			<div class="container-fluid" style="height: 100%">
-			<ul class="nav navbar-nav" style="width: 100%; height: 100%">
-			<div class="row align-items-center">
-<!--				<div class="col col-sm-1"></div>-->
-				<div class="col col-sm-3 text-center">
-				  <a class="navbar-brand" href="index.php" style="margin: auto">Tên E-Commerce</a>
-				</div>
-				<div class="col col-sm-6" style="padding: 0">
-					<input type="text" id="search" class="form-control" style="border-radius: 50px; background: #F0F0F0; border: 0px" placeholder="Tìm kiếm trên E-Commerce ....."/>
-				</div>
-				<div class="col col-sm-1" style="padding: 0">
-					<button type="submit" class="btn" style="color: black"><img src="img/icon/search.svg" width="20" height="20"></button>
-				</div>
-				<div class="col col-sm-2 text-center">
-					<a href="">
-					<img src="img/icon/cart.svg" width="32" height="32">
-					</a>
-				</div>
-			</ul>
-			</div>
-		</nav>
-	</div>
+   <nav class="navbar" style="padding: 5">
+      <div class="container-fluid" style="height: 100%">
+         <ul class="nav navbar-nav" style="width: 100%; height: 100%">
+            <div class="row align-items-center">
+               <!--				<div class="col col-sm-1"></div>-->
+               <div class="col col-sm-3 text-center">
+                  <a class="navbar-brand" href="index.php" style="margin: auto">Tên E-Commerce</a>
+               </div>
+               <div class="col col-sm-6" style="padding: 0">
+                  <input type="text" id="search" class="form-control"
+                     style="border-radius: 50px; background: #F0F0F0; border: 0px"
+                     placeholder="Tìm kiếm trên E-Commerce ....." />
+               </div>
+               <div class="col col-sm-1" style="padding: 0">
+                  <button type="submit" class="btn" style="color: black; box-shadow: none"><img
+                        src="img/icon/search.svg" width="20" height="20"></button>
+               </div>
+               <div class="col col-sm-2 text-center">
+                  <a href="">
+                     <img src="img/icon/cart.svg" width="32" height="32">
+                  </a>
+               </div>
+         </ul>
+      </div>
+   </nav>
+</div>
